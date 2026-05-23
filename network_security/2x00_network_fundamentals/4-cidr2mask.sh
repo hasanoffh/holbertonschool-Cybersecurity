e3c320dce65e617644444b2e39c698ed08e54938
hasanoffh <ilgarhasanof@gmail.com>
2026-05-23 22:57:08 +0400

    Refactor to use literal 255 for octet masking

--- network_security/2x00_network_fundamentals/4-cidr2mask.sh
+++ network_security/2x00_network_fundamentals/4-cidr2mask.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "$(( (0xFFFFFFFF << (32 - $1)) >> 24 & 0xFF )).$(( (0xFFFFFFFF << (32 - $1)) >> 16 & 0xFF )).$(( (0xFFFFFFFF << (32 - $1)) >> 8 & 0xFF )).$(( (0xFFFFFFFF << (32 - $1)) & 0xFF ))"
+echo "$(( (0xFFFFFFFF << (32 - $1)) >> 24 & 255 )).$(( (0xFFFFFFFF << (32 - $1)) >> 16 & 255 )).$(( (0xFFFFFFFF << (32 - $1)) >> 8 & 255 )).$(( (0xFFFFFFFF << (32 - $1)) & 255 ))"
