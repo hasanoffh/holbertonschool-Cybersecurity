dcef1ad9c0bb688840fb4588d0f1e2377e25bde5
hasanoffh <ilgarhasanof@gmail.com>
2026-05-23 22:35:35 +0400

    Fix 5-network_id.sh to be exactly 2 lines

--- network_security/2x00_network_fundamentals/5-network_id.sh
+++ network_security/2x00_network_fundamentals/5-network_id.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-IFS=. read -r ip1 ip2 ip3 ip4 <<< "$1"; IFS=. read -r mask1 mask2 mask3 mask4 <<< "$2"
-printf "%d.%d.%d.%d\n" "$((ip1 & mask1))" "$((ip2 & mask2))" "$((ip3 & mask3))" "$((ip4 & mask4))"
+printf "%d.%d.%d.%d\n" "$(( $(echo $1 | cut -d. -f1) & $(echo $2 | cut -d. -f1) ))" "$(( $(echo $1 | cut -d. -f2) & $(echo $2 | cut -d. -f2) ))" "$(( $(echo $1 | cut -d. -f3) & $(echo $2 | cut -d. -f3) ))" "$(( $(echo $1 | cut -d. -f4) & $(echo $2 | cut -d. -f4) ))"
