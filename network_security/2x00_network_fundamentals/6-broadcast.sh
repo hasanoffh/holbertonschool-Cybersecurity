993c269ca8211608deafb00f77cab5ab98b61625
hasanoffh <ilgarhasanof@gmail.com>
2026-05-23 22:37:45 +0400

    Fix 6-broadcast.sh to be exactly 2 lines with perfect stdout length

--- network_security/2x00_network_fundamentals/6-broadcast.sh
+++ network_security/2x00_network_fundamentals/6-broadcast.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-IFS=. read -r a b c d <<< "$1"; IFS=. read -r e f g h <<< "$2"
-out=$(printf "%d.%d.%d.%d" "$((a | (255 ^ e)))" "$((b | (255 ^ f)))" "$((c | (255 ^ g)))" "$((d | (255 ^ h)))"); echo -e "${out}\c"
+IFS=. read -r a b c d <<< "$1"; IFS=. read -r e f g h <<< "$2"; printf "%d.%d.%d.%d\n" "$((a | (255 ^ e)))" "$((b | (255 ^ f)))" "$((c | (255 ^ g)))" "$((d | (255 ^ h)))"
