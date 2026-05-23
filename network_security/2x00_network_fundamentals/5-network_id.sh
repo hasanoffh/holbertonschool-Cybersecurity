e1e7344445d20cd0835760237e7a14eec74e3305
hasanoffh <ilgarhasanof@gmail.com>
2026-05-23 22:36:07 +0400

    Fix 5-network_id.sh to include IFS and keep exactly 2 lines

--- network_security/2x00_network_fundamentals/5-network_id.sh
+++ network_security/2x00_network_fundamentals/5-network_id.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-printf "%d.%d.%d.%d\n" "$(( $(echo $1 | cut -d. -f1) & $(echo $2 | cut -d. -f1) ))" "$(( $(echo $1 | cut -d. -f2) & $(echo $2 | cut -d. -f2) ))" "$(( $(echo $1 | cut -d. -f3) & $(echo $2 | cut -d. -f3) ))" "$(( $(echo $1 | cut -d. -f4) & $(echo $2 | cut -d. -f4) ))"
+IFS=. read -r a b c d <<< "$1"; IFS=. read -r e f g h <<< "$2"; printf "%d.%d.%d.%d\n" "$((a & e))" "$((b & f))" "$((c & g))" "$((d & h))"
